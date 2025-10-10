351ea36c58f3b6f9bcbaf1815f1ad9d8c621299f
rudaoyi <rudaoyi@outlook.com>
2025-10-10 09:31:54 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-index.json   https://raw.githubusercontent.com/sdaoyi/my_m_ext/refs/heads/main/index.json
+index.json   https://raw.githubusercontent.com/sdaoyi/my_m_ext/refs/heads/main/index.min.json
 index.json   https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json
--- genid.py
+++ genid.py
@@ -1,7 +1,7 @@
 import json,hashlib,os
 
 def main():
-    with open(os.path.join(os.path.dirname(__file__),"index.json"),"r",encoding="utf-8") as f:
+    with open(os.path.join(os.path.dirname(__file__),"index.min.json"),"r",encoding="utf-8") as f:
         data=json.load(f)
     for ext in data:
         pkg=ext["pkg"]
@@ -13,7 +13,7 @@ def main():
             genid=hashlib.md5(genid.encode("utf-8")).hexdigest()
             genid=str(int(genid,16))[:19]
             s['id']=genid
-    with open(os.path.join(os.path.dirname(__file__),"index.json"),"w",encoding="utf-8") as f:
+    with open(os.path.join(os.path.dirname(__file__),"index.min.json"),"w",encoding="utf-8") as f:
         json.dump(data,f,ensure_ascii=False,indent=2)
 if __name__=="__main__":
     main()
