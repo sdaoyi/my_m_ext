b15d7fb9486b509d9eb4c55edb6718a8872933b5
rudaoyi <rudaoyi@outlook.com>
2025-10-10 09:19:18 +0800

    fix

---
+++ genid.py
@@ -0,0 +1,19 @@
+import json,hashlib,os
+
+def main():
+    with open(os.path.join(os.path.dirname(__file__),"index.json"),"r",encoding="utf-8") as f:
+        data=json.load(f)
+    for ext in data:
+        pkg=ext["pkg"]
+        ver=ext["version"]
+        code=ext["code"]
+        for s in ext["sources"]:
+            lang=s["lang"]
+            genid=f"{pkg}:{ver}:{code}:{lang}"
+            genid=hashlib.md5(genid.encode("utf-8")).hexdigest()
+            genid=str(int(genid,16))[:19]
+            s['id']=genid
+    with open(os.path.join(os.path.dirname(__file__),"index.json"),"w",encoding="utf-8") as f:
+        json.dump(data,f,ensure_ascii=False,indent=2)
+if __name__=="__main__":
+    main()
